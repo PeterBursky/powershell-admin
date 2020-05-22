cfd9ea0b91cd3e4619b2121d9e91dfb5d0f90a6f
Admin Peter <admin.peter@homelab.bursky.site>
2020-05-22 09:59:29 +0100

    Committer: Peter Bursky <peter.bursky@gmail.com>  Changes to be committed: 	modified:   README.md 	modified:   Update-ADSiteLink.ps1 Processing the input file

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # powershell-admin
 This repo contains useful administrative PowerShell scripts
 
-## Update-ADSiteLink
+### Update-ADSiteLink
 This script helps to automate updating Windows AD Site Links
--- Update-ADSiteLink.ps1
+++ Update-ADSiteLink.ps1
@@ -1,3 +1,55 @@
-#This script updates AD site links
-# Version 0.1
-#   initial version  
+<#
+    .SYNOPSIS
+        The goal of this script is to update the AD Site Links according to information provided in a CSV file.
+
+    .DESCRIPTION
+        The goal of this script is to update the AD Site Links according to information provided in a CSV file.
+
+    .PARAMETER AdSitesCsv
+        Specifies the CSV file containing the list of DNS  servers to be updated
+        The CSV file should be without headers
+
+    .EXAMPLE
+        ./Update-DnsForwarder.ps1 -DnsServersCsv ./DnsServers.csv -AddressToTest "www.bursky.net" -DnsForwarder 9.9.9.9
+
+        This example will query all DNS servers from the CSV file DnsServers.csv, trying to resolve the address WWW.BURSKY.NET using the DNS forwarder 9.9.9.9
+
+    .NOTES
+        NAME:   Update-AdSiteLink.ps1
+        AUTHOR: Peter Bursky
+        DATE:   21/5/2020
+        EMAIL:  peter@bursky.net
+
+        REQUIREMENTS:
+        -Administrator permissions
+
+        VERSION HISTORY:
+        0.1 2020.05.21
+            Initial Version.
+    .link
+        https://github.com/PeterBursky/powershell-admin
+#>
+
+[CmdletBinding()]
+PARAM (
+    [Parameter(Mandatory = $true, HelpMessage = "You must specify the CSV file containg the AD Site Link details")]
+    [String]$AdSitesCsv
+)
+
+Write-Verbose "Importing CSV file containg the AD Site Configurations"
+$adSiteList = Import-Csv -Path $AdSitesCsv -Header LinkName,Site1,Site2,Cost
+$csvFileName = [System.IO.Path]::GetFileNameWithoutExtension($AdSitesCsv) + "_output.csv"
+$csvText = "LinkName,Result,Details"
+$csvText | Out-File -FilePath $csvFileName
+
+foreach ($adSite in $adSiteList) {
+    Write-Verbose "Processing record: $adSite"
+    try {
+        $result = Get-ADReplicationSite -Identity $adSite.Site1
+        $result = Get-ADReplicationSite -Identity $adSite.Site2
+    } #TRY
+    catch {
+        Write-Verbose "AN error occurred:"
+        Write-Verbose $_
+    }
+} #FOREACH
